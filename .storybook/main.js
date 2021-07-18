336fb225a0d9551354e9b1c424c076000d9a9816
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-07-19 00:05:52 +0100

    config webpack to load css loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,4 +7,23 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-postcss",
   ],
+
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            postcssOptions: {
+              plugins: [require("tailwindcss"), require("autoprefixer")],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
 };
