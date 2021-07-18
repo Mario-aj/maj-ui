75f120cd024c3223c8bb375c88a45e8088224a7f
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-07-19 00:04:34 +0100

    remove webpack config

--- src/components/index.stories.tsx
+++ src/components/index.stories.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ComponentStory } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Button } from ".";
 
 export default {
   title: "Teste/Button",
   component: Button,
   parameters: {},
-};
+} as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args: any) => (
   <Button {...args} />
