9788030aa409378c2be03b74515c3045141a4264
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-07-21 10:21:08 +0100

    apply animation

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory } from '@storybook/react';
 
 import { Modal, ModalProps } from '.';
@@ -22,4 +22,45 @@ export const Base = Template.bind({});
 Base.args = {
   children: 'Welcome to my modal component',
   isOpen: true,
+};
+
+export const InterativeModal = (): JSX.Element => {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+
+  const handleTransactionModalOpen = () => {
+    setIsTransactionModalOpen(true);
+  };
+
+  const handleTransactionModalClose = () => {
+    setIsTransactionModalOpen(false);
+  };
+
+  return (
+    <div className="flex-col items-center justify-center w-full">
+      <button
+        className="px-2 py-1 text-white bg-red-400 rounded"
+        onClick={handleTransactionModalOpen}
+      >
+        Open modal
+      </button>
+      <Modal
+        isOpen={isTransactionModalOpen}
+        onClose={handleTransactionModalClose}
+      >
+        <div>
+          <header>
+            <p>Income</p>
+          </header>
+          <strong>$1000,00</strong>
+        </div>
+
+        <div>
+          <header>
+            <p>Outcome</p>
+          </header>
+          <strong>-$2000,00</strong>
+        </div>
+      </Modal>
+    </div>
+  );
 };
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -32,14 +32,20 @@ export const Modal = ({
   return (
     <div
       className={classnames(
-        'fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-gray-600',
+        'fixed top-0 bottom-0 left-0 right-0 opacity-0 pointer-events-none transition-all duration-300 ease-in-out flex items-center justify-center bg-gray-600',
         {
-          'opacity-0 pointer-events-none': !isOpen,
           'opacity-100 pointer-events-auto': isOpen,
         }
       )}
     >
-      <div className="relative flex flex-col p-4 bg-white rounded-md">
+      <div
+        className={classnames(
+          'relative flex flex-col p-4 bg-white -translate-y-52 rounded-md box-border transition-all duration-300 ease-in-out ',
+          {
+            'translate-y-0': isOpen,
+          }
+        )}
+      >
         <button
           className="absolute px-3 py-1 text-white transition-opacity duration-300 bg-gray-500 rounded-full cursor-pointer top-1 right-1 bg-none opacity-80 hover:opacity-100 focus:outline-none"
           onClick={onClose}
