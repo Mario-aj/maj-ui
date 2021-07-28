cf7beec5c3cdbfbd92a414eb45ef41935cf0ed5c
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-07-28 11:48:05 +0100

    write doc of button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Button, ButtonProps } from '.';
 
+import docs from './Button.doc.mdx';
+
 export default {
   title: 'Components/Button',
   component: Button,
   parameters: {
     layout: 'centered',
+    docs: {
+      page: docs,
+    },
   },
   argTypes: {
     label: 'string',
