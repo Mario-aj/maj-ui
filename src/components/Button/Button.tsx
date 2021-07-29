00e7ca1e67bdde1215509a1f82244fefa9df06dd
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-07-29 22:27:32 +0100

    refactoring intent

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import classnames from 'classnames';
+import { INTENT_CLASS_Map } from './intent';
 
 export interface ButtonProps extends HTMLButtonElement {
   /*
@@ -33,39 +34,6 @@ export interface ButtonProps extends HTMLButtonElement {
   */
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
-
-const INTENT_CLASS_Map = {
-  base: 'px-4 py-1.5 text-sm transition-all duration-300 rounded flex items-center justify-center',
-  secondary: {
-    normal: 'bg-gray-100 hover:bg-gray-200 active:bg-gray-300',
-    outline: 'bg-white border border-gray-500',
-  },
-  primary: {
-    normal: 'bg-blue-600 hover:bg-blue-500 active:bg-blue-800 text-white',
-    outline:
-      'bg-white text-blue-500 border border-blue-500 hover:bg-blue-200 active:border-0 active:bg-blue-100 active:text-blue-700',
-  },
-  success: {
-    normal: 'bg-green-600 hover:bg-green-500 active:bg-green-800 text-white',
-    outline:
-      'bg-white text-green-500 border border-green-500 hover:bg-green-200 active:border-0 active:bg-green-100 active:text-green-700',
-  },
-  warning: {
-    normal: 'bg-yellow-600 hover:bg-yellow-500 active:bg-yellow-800 text-white',
-    outline:
-      'bg-white text-yellow-500 border border-yellow-500 hover:bg-yellow-200 active:border-0 active:bg-yellow-100 active:text-yellow-700',
-  },
-  danger: {
-    normal: 'bg-red-600 hover:bg-red-500 active:bg-red-800 text-white',
-    outline:
-      'bg-white text-red-500 border border-red-500 hover:bg-red-200 active:border-0 active:bg-red-100 active:text-red-700',
-  },
-  dark: {
-    normal: 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 text-white',
-    outline:
-      'bg-gray-800 text-white border border-white hover:bg-gray-500 active:border-0 active:bg-gray-600',
-  },
-};
 
 export const Button = ({
   disabled = false,
