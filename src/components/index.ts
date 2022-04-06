88589c0e2202e44db7c0ab9c89d9ce70093845ef
mario-aj <mariojorge1997@mail.ru>
2022-04-06 14:24:45 +0100

    remove all

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Modal } from './Modal';
-export { Button } from './Button';
-export { Spinner } from './Spinner';
--- styles/intents.ts
+++
@@ -1,6 +0,0 @@
-export const INTENT_CLASSES = {
-  primary: '#137CBD',
-  success: '#0f9960',
-  danger: '#DB3737',
-  secondary: '#5C7080',
-};
