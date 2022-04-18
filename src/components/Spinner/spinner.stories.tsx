cf8a29c1fe9ac4580beeaabf814cffa22cb47f48
mario-aj <mariojorge1997@mail.ru>
2022-04-18 17:01:38 +0100

    chore: write story for spinner

---
+++ src/components/Spinner/spinner.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import {} from 'react';
+
+import { Spinner } from '.';
+
+export default {
+  title: 'Components/Spinner',
+  component: Spinner,
+} as ComponentMeta<typeof Spinner>;
+
+const Template: ComponentStory<typeof Spinner> = args => <Spinner {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  size: 'lg',
+  appearance: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  appearance: 'secondary',
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  appearance: 'success',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  appearance: 'warning',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  appearance: 'danger',
+};
+
+export const Sizes: ComponentStory<typeof Spinner> = () => (
+  <div className="flex items-center gap-4">
+    <Spinner appearance="primary" size="sm" />
+    <Spinner appearance="success" size="md" />
+    <Spinner appearance="danger" size="lg" />
+  </div>
+);
