ca97bce2f98665b252223af99e34dabdc45627a6
mario-aj <mariojorge1997@mail.ru>
2022-04-20 11:15:53 +0100

    feat: color file

--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -2,21 +2,13 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { ProgressBarProps } from '.';
+import { colors } from '../../styles';
 
 type WrapperProps = React.HTMLAttributes<HTMLDivElement> &
   Pick<ProgressBarProps, 'size'>;
 
 type ProgressProps = React.HTMLAttributes<HTMLDivElement> &
   Pick<ProgressBarProps, 'appearance' | 'value'>;
-
-const classes = {
-  primary: '#5F91CC',
-  secondary: '#969696',
-  success: '#5BCA8A',
-  warning: '#F0B858',
-  danger: '#D6394C',
-  light: '#fff',
-};
 
 const sizes = {
   sm: '0.25rem',
@@ -47,7 +39,7 @@ const progressStyle = ({
 
   border-radius: 0.5rem;
   overflow: hidden;
-  background-color: ${classes[appearance]};
+  background-color: ${colors[appearance].default};
 `;
 
 export const Wrapper = styled('div')<WrapperProps>(wrapperStyle);
