cd04c389a832479bb1e3d34e0a15ad2b5f0a4f9d
mario-aj <mariojorge1997@mail.ru>
2022-04-20 14:53:11 +0100

    remove tailwind

--- stories/progress-bar.stories.tsx
+++ stories/progress-bar.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { ProgressBar } from '.';
+import { ProgressBar } from '../src';
 
 export default {
   title: 'Components/ProgressBar',
