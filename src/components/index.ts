049c4fdf6bcd4a944215739a89914b4cd89ce8ba
mario-aj <mariojorge1997@mail.ru>
2022-04-20 10:36:14 +0100

    refactor: adjust button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 export { Button } from './Button';
-export type { ButtonProps } from './Button/types';
+export type { ButtonProps } from './Button';
 
 export { Spinner } from './Spinner';
 export type { SpinnerProps } from './Spinner';
+
+export { ProgressBar } from './ProgressBar';
+export type { ProgressBarProps } from './ProgressBar';
