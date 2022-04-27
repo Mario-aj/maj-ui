a6b1db13d99fb6c18b8796cfb23cd56da466ab2f
mario-aj <mariojorge1997@mail.ru>
2022-04-27 18:52:10 +0100

    doc: wrtite stories

---
+++ src/components/AlertMessage/alert-message.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { MdCheck } from 'react-icons/md';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { AlertMessage } from '.';
+
+export default {
+  title: 'Components/AlertMessage',
+  component: AlertMessage,
+} as ComponentMeta<typeof AlertMessage>;
+
+const Template: ComponentStory<typeof AlertMessage> = args => (
+  <AlertMessage {...args} />
+);
+
+export const Info = Template.bind({});
+Info.args = {
+  message: 'This is an info message',
+  type: 'info',
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  message: 'This is a success message',
+  type: 'success',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  message: 'This is a warning message',
+  type: 'warning',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  message: 'This is an error message',
+  type: 'error',
+};
+
+export const WithTitle = Template.bind({});
+WithTitle.args = {
+  message: 'This is an error message with title',
+  type: 'error',
+  title: 'Error title',
+};
+
+// export const WithCloseButton = Template.bind({});
+// WithCloseButton.args = {
+//   message: 'This is an error message with close button',
+//   type: 'error',
+//   onClose: () => {
+//     alert('Close button clicked');
+//   },
+// };
+
+export const WithCustomIcon = Template.bind({});
+WithCustomIcon.args = {
+  message: 'This is an error message with close button',
+  type: 'success',
+  icon: <MdCheck />,
+  onClose: undefined,
+};
