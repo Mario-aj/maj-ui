008e06c1a79510883a64f66c697850aaf22192e8
mario-aj <mariojorge1997@mail.ru>
2022-05-20 17:23:32 +0100

    configt jest coverage limit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  coverageThreshold: {
+    global: {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85,
+    },
+  },
 };
